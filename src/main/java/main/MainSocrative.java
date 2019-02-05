1f6320cd15ed09651744201245e1359e18b6b86f
alfonso <ta.ge@hotmail.es>
2019-02-05 11:53:21 -0500

    arreglo del main

--- src/main/java/main/MainSocrative.java
+++ src/main/java/main/MainSocrative.java
@@ -1,17 +1,10 @@
 package main;
 
-import java.util.Scanner;
-
 public class MainSocrative {
 
 	public static void main(String[] args) {
-		/*Scanner data = new Scanner(System.in);
-		var ent = data.nextInt();
-		if (ent == 1) {*/
-			//new ProfWSSocrative().inicia();
-		//} else {
-			new AlumWSSocrative().inicia();
-		//}
+		//new ProfWSSocrative().inicia();
+		new AlumWSSocrative().inicia();
 
 	}
 
