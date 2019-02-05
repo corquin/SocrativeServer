113912567a8d32ecf7b092a5ed0e584267ee0492
alfonso <ta.ge@hotmail.es>
2019-02-05 11:57:37 -0500

    socrati_v2

--- src/main/java/main/MainSocrative.java
+++ src/main/java/main/MainSocrative.java
@@ -3,8 +3,8 @@ package main;
 public class MainSocrative {
 
 	public static void main(String[] args) {
-		//new ProfWSSocrative().inicia();
-		new AlumWSSocrative().inicia();
+		new ProfWSSocrative().inicia();
+		//new AlumWSSocrative().inicia();
 
 	}
 
